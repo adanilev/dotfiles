76aa6194971fcf3a8a7fc9421cbb8bccca6edbd2
Alex Danilevsky <adanilev@users.noreply.github.com>
2020-02-04 00:30:33 +1100

    feat: add install script + ts plugin

---
+++ install.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+set -o nounset    # error when referencing undefined variable
+set -o errexit    # exit when command fails
+
+# https://github.com/neoclide/coc.nvim/wiki/Install-coc.nvim
+function installCoc() {
+  echo "installing coc.nvim"
+  mkdir -p ~/.vim/pack/plugins/start
+  cd ~/.vim/pack/plugins/start
+  curl --fail -L https://github.com/neoclide/coc.nvim/archive/release.tar.gz | tar xzfv -
+
+  # install extensions
+  mkdir -p ~/.config/coc/extensions
+  cd ~/.config/coc/extensions
+  if [ ! -f package.json ]
+  then
+    echo '{"dependencies":{}}'> package.json
+  fi
+
+  # add more extensions here as required
+  npm install \
+    coc-highlight \
+    coc-html \
+    coc-json \
+    coc-tsserver \
+    coc-yaml \
+    --global-style --ignore-scripts --no-bin-links --no-package-lock --only=prod
+}
+
+# TODO: install nvm
+installCoc
