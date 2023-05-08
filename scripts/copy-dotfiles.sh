1e6c9444033cc17d5b4c97e163a6c430bbaf69d4
Alex Danilevsky <adanilev@users.noreply.github.com>
2023-05-08 18:35:55 +1000

    feat: add nvim config

--- scripts/copy-dotfiles.sh
+++ scripts/copy-dotfiles.sh
@@ -1,14 +1,22 @@
 #!/usr/bin/env bash
 
-set -euox pipefail
+set -euxo pipefail
+
+DOTFILES_DIR=dotfiles
+NVIM_DIR=nvim
 
 # git - linked in `make dotfiles`, don't link so zshrc_work can overwrite
-cp ${DOTFILES_DIR}/gitconfig ${DOTFILES_DIR}/.gitconfig
+cp "${DOTFILES_DIR}"/gitconfig "${DOTFILES_DIR}"/.gitconfig
 
-for dotfile in $(find ${DOTFILES_DIR} -maxdepth 1 -name ".*"); do
+# link the dotfiles
+find "${DOTFILES_DIR}" -maxdepth 1 -name ".*" | while read -r dotfile; do
   dotfile_path=$(readlink -f "${dotfile}")
   dotfile_name=$(basename "${dotfile}")
   dotfile_link_path="${HOME}"/"${dotfile_name}"
 
   ln -sfn "${dotfile_path}" "${dotfile_link_path}"
 done
+
+# link the nvim
+nvim_dir_path=$(readlink -f "${NVIM_DIR}")
+ln -sfn "${nvim_dir_path}" "${HOME}"/.config/nvim
